0cb1f7ab2d50c6cf64aa412c459ed82479b73cf7
ChocoFanta-01 <ljh970512@naver.com>
2026-03-27 17:22:28 +0900

    첫 강의

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/common/Header";
+
+function Home() {
+    return (
+        <>
+            <Header />
+            <div>book body</div>
+        </>
+    );
+}
+export default Home;
