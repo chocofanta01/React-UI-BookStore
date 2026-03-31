053d061e653f23f01cc26841330ecd4f15148825
ChocoFanta-01 <ljh970512@naver.com>
2026-03-31 21:07:03 +0900

    기본 컴포넌트와 테스트, 헤더와 푸터 구현

--- src/style/global.ts
+++ src/style/global.ts
@@ -1,15 +1,29 @@
-import "sanitize.css";
-import { createGlobalStyle } from "styled-components";
+import 'sanitize.css';
+// global style 지정
+import { createGlobalStyle } from 'styled-components';
+import { ThemeName } from './theme';
 
-const GlobalStyle = createGlobalStyle`
-   body{
-    margin: 0;
-    padding: 0;
-   } 
-    
-   h1{
-    margin: 0;
-   }
+
+
+interface Props {
+    // theme.ts에서 작성한 ThemeName 타입으로 light와 dark로 제한!
+    themeName: ThemeName;
+}
+
+
+export const GlobalStyle = createGlobalStyle<Props>`
+    body {
+        padding: 0;
+        margin: 0;
+        background-color: ${(props) => (props.themeName === 'light' ? 'white' : 'black')};
+    }
+
+    h1 {
+        margin: 0;
+    }
+
+    /* 모든 엘리먼트에 적용 */
+    * {
+        color: ${(props) => (props.themeName === 'light' ? 'black' : 'white')}
+    }
 `
-
-export default GlobalStyle;
